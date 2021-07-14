4c46b01a6590303167e746429541d2be13392e2a
benjaminearl <bnjmnearl@gmail.com>
2021-07-14 11:17:40 +0200

    change colour for waterschool

--- canvas.js
+++ canvas.js
@@ -44,10 +44,10 @@ resizeCanvas()
 function update() {
     setTimeout(() => {
         //Draw the canvas background
-        ctx.fillStyle ='red';
+        ctx.fillStyle ='#3fa7d5';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         //Draw face Logo and his background
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = '#3fa7d5';
         ctx.fillRect(face.x, face.y, face.width*scale, face.height*scale);
         ctx.drawImage(face.img, face.x, face.y, face.width*scale, face.height*scale);
         //Move the logo
