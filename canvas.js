6ee34326b100499d9bfbb995843a864af10f01b8
benjaminearl <bnjmnearl@gmail.com>
2022-09-21 16:31:49 +0200

    turn website back to red and add GRAW info

--- canvas.js
+++ canvas.js
@@ -44,10 +44,10 @@ resizeCanvas()
 function update() {
     setTimeout(() => {
         //Draw the canvas background
-        ctx.fillStyle ='green';
+        ctx.fillStyle ='red';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         //Draw face Logo and his background
-        ctx.fillStyle = 'green';
+        ctx.fillStyle = 'red';
         ctx.fillRect(face.x, face.y, face.width*scale, face.height*scale);
         ctx.drawImage(face.img, face.x, face.y, face.width*scale, face.height*scale);
         //Move the logo
