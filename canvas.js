bbc21b6b3388a642e47101122caa0075b1a478ee
benjaminearl <bnjmnearl@gmail.com>
2025-09-20 12:06:49 +0200

    MAKE IT BLUE

--- canvas.js
+++ canvas.js
@@ -44,10 +44,10 @@ resizeCanvas()
 function update() {
     setTimeout(() => {
         //Draw the canvas background
-        ctx.fillStyle ='red';
+        ctx.fillStyle ='#0011CD';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         //Draw face Logo and his background
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = '#0011CD';
         ctx.fillRect(face.x, face.y, face.width*scale, face.height*scale);
         ctx.drawImage(face.img, face.x, face.y, face.width*scale, face.height*scale);
         //Move the logo
